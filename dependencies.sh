948e2f6bc0c367e2c5f9061efa1bf69347ba4fbf
Lukas Martinelli <me@lukasmartinelli.ch>
2014-03-14 17:19:42 +0100

    Using lib dir for building dependencies now

--- dependencies.sh
+++ dependencies.sh
@@ -1,22 +1,22 @@
+mkdir lib
+cd lib
+
 # Install fs (pyfilesystem)
-echo "Checking out pyfilesystem to install it"
+echo "Checking out pyfilesystem with svn to install it"
 svn checkout http://pyfilesystem.googlecode.com/svn/trunk/ pyfilesystem
 cd pyfilesystem
 echo "Installing pyfilesystem..."
 python setup.py install
 echo "Installed pyfilesystem"
 cd ..
-rm -r pyfilesystem
-echo "Cleaned up pyfilesystem"
-
 
 # Install amazon-cloud-drive
-echo "Checking out amazon-cloud-drive to install it"
+echo "Checking out amazon-cloud-drive with svn to install it"
 svn checkout http://pyamazonclouddrive.googlecode.com/svn/trunk/ amazon-cloud-drive
 echo "Installing amazon-cloud-drive..."
 cd amazon-cloud-drive
 echo "Installed amazon-cloud-drive"
 python setup.py install
 cd ..
-rm -r amazon-cloud-drive
-echo "Cleaned up amazon-cloud-drive"
+
+cd ..
