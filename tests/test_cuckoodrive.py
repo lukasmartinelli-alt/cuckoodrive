26025084cf5ef5ad728ae2a56e5b5bfc9e1e89fe
Lukas Martinelli <me@lukasmartinelli.ch>
2014-03-16 19:59:22 +0100

    Tried to work on StorageBroker

--- tests/test_cuckoodrive.py
+++ tests/test_cuckoodrive.py
@@ -1,35 +1,73 @@
 from __future__ import print_function, division, absolute_import, unicode_literals
-from os import urandom
 
 from pytest import fixture
 
 from fs.memoryfs import MemoryFS
 
-from drive.cuckoodrive import CuckooDriveFS
+
+class StorageProvider:
+    pass
+
+
+class StorageSizeError(Exception):
+    pass
+
+
+class StorageBroker:
+    """The StorageBroker decides where files are saved and where they can broker
+    read from"""
+
+    def __init__(self, providers):
+        self.providers = providers
+
+    def write(self, filesize):
+        """Ask the broker where to write a file with the given filesize"""
+
+        def free_space(provider):
+            return provider.getmeta("free_space", 0)
+
+        print([free_space(location) for location in self.providers])
+        best_location = max(self.providers, key=free_space)
+        if(filesize < best_location):
+            return [(best_location, (0, filesize))]
+        else:
+            raise StorageSizeError()
 
 
 class TestCuckooDriveFS:
     """Test implementation of the CuckooDriveFS"""
-    @fixture
-    def providers():
-        dropbox = MemoryFS()
-        googledrive = MemoryFS()
-        onedrive = MemoryFS()
-        mega = MemoryFS()
-        return [dropbox, googledrive, onedrive, mega]
+    pass
 
-    @fixture
-    def fs():
-        return CuckooDriveFS()
 
-    def open_write_large_file_should_split_into_chunks(self, fs):
-        chunk_size = 4194304
-        buffer_size = 1024
+def MB(size):
+    return size * 1024 * 1024
+
+
+class TestStorageBroker:
+    @fixture
+    def broker(self):
+        self.dropbox = MemoryFS()
+        self.dropbox._meta["free_space"] = MB(230)
+        self.googledrive = MemoryFS()
+        self.googledrive._meta["free_space"] = MB(120)
+
+        return StorageBroker(providers=[self.dropbox, self.googledrive])
+
+    def test_write_small_file_returns_location(self, broker):
         #Arrange
-        path = "backup.zip"
+        filesize = MB(30)
         #Act
-        with fs.open(path, "w") as file:
-            for _ in range(0, chunk_size / buffer_size):
-                file.write(urandom(buffer_size))
+        locations = broker.write(filesize)
         #Assert
-        assert ["backup.zip.part1", "backup.zip.part2"] == fs.listdir()
+        assert (self.dropbox, (0, filesize)) == locations[0]
+
+    def test_write_big_file_returns_multiple_locations_where_to_write_what(self, broker):
+        #Arrange
+        filesize = MB(330)
+        #Act
+        locations = broker.write(filesize)
+        #Assert
+        assert locations == [(self.googledrive, (MB(0), MB(100))),
+                             (self.dropbox, (MB(100), MB(200))),
+                             (self.dropbox, (MB(200), MB(300))),
+                             (self.dropbox, (MB(300), MB(330)))]
