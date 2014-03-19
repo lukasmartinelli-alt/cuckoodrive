d92680de7a3e75ec5199649ac2475f1567ac9b7a
Lukas Martinelli <me@lukasmartinelli.ch>
2014-03-19 21:28:04 +0100

    Added more tests

--- tests/test_cuckoofs.py
+++ tests/test_cuckoofs.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import, unicode_literals
 from fs.base import FS
+from fs.path import normpath
 from pytest import fixture
 
 from dropbox.client import DropboxClient
@@ -11,6 +12,42 @@ from tests.test_fs import FSTestBase
 
 
 class CuckooFS(FS):
+    """
+    Writes to many underlying storage providers implemented as remote filesystems.
+    """
+    _meta = {
+        "network": True,
+        "virtual": True,
+        "read_only": False,
+        "unicode_paths": True,
+        "case_insensitive_paths": False,
+        "atomic.move": True,
+        "atomic.copy": True,
+        "atomic.makedir": True,
+        "atomic.rename": True,
+        "atomic.setcontents": True,
+        "file.read_and_write": False,
+    }
+
+    def __str__(self):
+        return "<CuckooFS: {0}>".format(self.root_path)
+
+    # --------------------------------------------------------------------
+    # Essential Methods as defined in
+    # https://pythonhosted.org/fs/implementersguide.html#essential-methods
+    # --------------------------------------------------------------------
+    @synchronize
+    def open(self, path, mode="rb", **kwargs):
+        path = normpath(path).lstrip('/')
+
+        if "r" in mode:
+            if not self.exists(path):
+                raise ResourceNotFoundError(path)
+            if self.isdir(path):
+                raise ResourceInvalidError(path)
+        file = DropboxFile(self, path, mode)
+        return file
+
     # ------------------------------------------------------------------------
     # Non-Essential Methods as defined in
     # https://pythonhosted.org/fs/implementersguide.html#non-essential-methods
