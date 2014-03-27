35ac94466cc4f422fca37e77be63df02c2fe36ba
Lukas Martinelli <me@lukasmartinelli.ch>
2014-03-27 17:47:17 +0100

    Implemented basic functionality for fs.

--- drive/partedfs.py
+++ drive/partedfs.py
@@ -7,10 +7,30 @@ from fs.path import dirname, basename, splitext
 from fs.wrapfs import WrapFS, wrap_fs_methods
 
 
+
+
+
 class PartedFS(WrapFS):
     """
     A virtual filesystem that splits large files into many smaller files.
     This filesystem uses an underlying filesystem to translate the many small files back and forth.
+
+    The process actually takes a large file and splits it into parts with a max size.
+    So a file like 'backup.tar' in the folder '/backups' with 240 MB will translate into following files::
+
+    `-- backups
+        |-- backup.tar.part0 (100MB)
+        |-- backup.tar.part1 (100MB)
+        `-- backup.tar.part2 (40MB)
+
+    If the max part size would have been set to 300 MB, there will only be a single file, but for simplicity sake,
+    still managed as a part (so we can easily extend the file later)::
+
+    `-- backups
+        `-- backup.tar.part0 (240MB)
+
+    One problem is, that we never know wether a file is complete, because there might
+    be one missing part.
     """
 
     _meta = {
@@ -29,6 +49,22 @@ class PartedFS(WrapFS):
         self.max_part_size = max_part_size
         super(PartedFS, self).__init__(fs)
 
+    def _encode(self, path):
+        """
+        Add the .part0 extension to the given path
+        :param path: The plain path to encode
+        :returns Encoded path (without .part extension)
+        """
+        return "{0}.part0".format(path)
+
+    def _decode(self, path):
+        """
+        Remove the .part[0-9] extension from the given path
+        :param path: Encoded path
+        :returns Decoded path (added .part extension)
+        """
+        return splitext(path)[0]
+
     def listparts(self, path, full=False, absolute=False):
         """
         Return all parts for a given path.
@@ -38,15 +74,10 @@ class PartedFS(WrapFS):
         return self.wrapped_fs.listdir(path=dirname(path), wildcard="{0}.part*".format(basename(path)),
                                        full=full, absolute=absolute, files_only=True)
 
-    @iotools.filelike_to_stream
-    def open(self, path, mode='r', **kwargs):
-        parts = [FilePart(open_part(part), mode, self.max_part_size) for part in self.listparts(path)]
-
-        return PartedFile(path, mode, self.wrapped_fs, self.max_part_size)
-
     def remove(self, path):
         """
         Remove a virtual file with path from the filesystem. This will delete all associated paths.
+        :param path: Raw path of where to remove all the associated paths
         """
         for part in self.listparts(path):
             self.wrapped_fs.remove(part)
@@ -56,10 +87,9 @@ class PartedFS(WrapFS):
         Lists the file and directories under a given path. This will return all .part0 files in the underlying fs
         as files and the other normal dirs as dirs.
         """
-        print("LISTDIR")
         dirs = self.wrapped_fs.listdir(path=path, dirs_only=True, wildcard=wildcard, full=full, absolute=absolute)
         files = self.wrapped_fs.listdir(path=path, files_only=True, wildcard="*.part0", full=full, absolute=absolute)
-        files = [splitext(f)[0] for f in files]
+        files = [self._decode(f) for f in files]
         if dirs_only:
             return dirs
         if files_only:
@@ -67,228 +97,23 @@ class PartedFS(WrapFS):
         return dirs + files
 
     def exists(self, path):
-        """Check wether a path exists. Please use the virtual file names (without .part)"""
-        return self.wrapped_fs.exists(path) or self.wrapped_fs.exists("{0}.part0".format(path))
+        """
+        Check wether the encoded path exists on the wrapped_fs. If the normal path exists as well
+        it is a directory and should also return True
+        :param path: Path that will be encoded and checked wether it exists
+        """
+        return self.wrapped_fs.exists(path) or self.wrapped_fs.exists(self._encode(path))
 
     def isfile(self, path):
-        return self.wrapped_fs.isfile("{0}.part0".format(path))
+        """
+        Check wether the encoded path is a file on the wrapped_fs
+        """
+        return self.wrapped_fs.isfile(self._encode(path))
 
 
 class PartSizeExceeded(Exception):
     pass
 
 
-class FilePart(FileWrapper):
-    """
-    A part of a file (PartedFile) that can reach a maximum size and then must be extended to another part.
-    """
-
-    def __init__(self, wrapped_file, mode, max_size):
-        """
-        Create a new FilePart by wrapping it around an existing file.
-        :param wrapped_file: The existing file to wrap around
-        :param mode: The mode the wrapped_file was originally opened.
-        :param max_size: The max size this part can reach.
-        """
-        super(FilePart, self).__init__(wrapped_file, mode)
-        self.max_size = max_size
-        self.size = 0
-        self._file_pointer = 0
-
-    def fill(self, data):
-        """
-        Fill the part up with the given data and return the data that could not be written.
-        If all the data has been written it returns None.
-
-        This might look similar to the standard IO convention, but we can't rely always on this implementation.
-
-        :param data: The data to write to the part
-        :return all data that could not be filled into this part or None if all data fit in
-        """
-        space_left = self.max_size - (self.tell() % self.max_size)
-        if len(data) < space_left:
-            self.write(data)  # TODO: It could be that the data cannot be all written, this case should be handled
-            return None
-        else:
-            self.write(data[0:space_left])
-            return data[space_left:]
-
-    def _write(self, data, flushing=False):
-        self._file_pointer += len(data)
-        if self._file_pointer > self.size:
-            self.size = self._file_pointer
-        if self.size > self.max_size:
-            raise PartSizeExceeded("Part cannot grow bigger than {0}.".format(self.max_size))
-
-        self.wrapped_file.write(data)
-
-    def _seek(self, offset, whence):
-        self._file_pointer = offset
-        self.wrapped_file.seek(offset, whence)
-
-
-class InvalidFilePointerLocation(Exception):
-    """
-    An error that happens when the file pointer is in a negative offset or at a place where there
-    is no part for it
-    """
+class FilePart(object):
     pass
-
-
-class PartedFile(FileLikeBase):
-    """
-    A PartedFile is composed out of many other smaller files (FilePart).
-    """
-
-    def __init__(self, path, mode, fs, max_part_size, parts=None):
-        """
-        Create a PartedFile for a path.
-        :param parts: File handles to the associated parts
-        :param path: Path of the virtual file
-        :param mode: Mode with which the file was opened and with thich all the part_paths should be opened as well
-        :param fs: Filesystem where this PartedFile exists
-        :param max_part_size: The max a part of the file can reach
-        """
-        super(PartedFile, self).__init__()
-        self.max_part_size = max_part_size
-
-        self._path = path
-        self._mode = mode
-        self._fs = fs
-        self._fpointer = 0  # Current position of the file pointer
-        self._parts = parts if parts else []
-
-    def _data_too_big(self, data):
-        """Returns True if there is any data and it is longer than the left space in the file"""
-        return data and self._fpointer + len(data) > self.max_part_size
-
-    @property
-    def current_part(self):
-        """
-        Calculates the current part by looking up in which part the file pointer must be.
-        If there are no parts it returns None.
-
-        If you've filled a part with all possible bytes and advances the _fpointer, the current_part
-        will still be the part you've written to. To advance to the next part you should use _next_part or _expand
-        """
-        if len(self._parts) == 0:
-            return None
-
-        size = 0
-        for part in self._parts:
-            size += self.max_part_size
-            if size >= self._fpointer:
-                return part
-
-        raise InvalidFilePointerLocation("File pointer points to a location that is not part of the file.")
-
-    def _next_part(self):
-        """Open and return the next part of the current_part"""
-        next_index = self._parts.index(self.current_part) + 1
-        if next_index < len(self._parts):
-            next_part = self._parts[next_index]
-            if next_part.closed:
-                next_part.fs.open(next_part.path, next_part.mode)
-            return next_part
-        else:
-            return None
-
-    def _expand_part(self):
-        """Expand the current_part to a new file and return it."""
-        path = self._path + ".part{0}".format(len(self._parts))
-        wrapped_part = self._fs.open(path, mode=self._mode)
-        new_part = FilePart(wrapped_part, self._mode, self.max_part_size)
-        self._parts.append(new_part)
-        return new_part
-
-    def _eof_reached(self):
-        return self._fpointer == sum(part.size for part in self._parts)
-
-    def _readall(self):
-        all_data = bytes()
-        for part in self._parts:
-            part_data = part.read()
-            self._fpointer += len(part_data)
-            all_data += part_data
-        return all_data
-
-    def _readbuffered(self, sizehint):
-        read_space = (self._fpointer % self.max_part_size)
-
-        if read_space + sizehint > self.current_part.size:
-            part_data = self.current_part.read(self.current_part.size - read_space)
-            self._fpointer += len(part_data)
-
-            next_part = self._next_part()
-
-            if not next_part:
-                return part_data
-
-            next_part.seek(offset=0, whence=0)
-            next_part_data = next_part.read(sizehint - len(part_data))
-            self._fpointer += len(next_part_data)
-            return part_data + next_part_data
-        else:
-            part_data = self.current_part.read(sizehint)
-            self._fpointer += len(part_data)
-            return part_data
-
-    # --------------------------------------------------------------------
-    # Essential Methods that are expected by the FileLikeBase
-    # to be implemented and should behave as defined
-    # --------------------------------------------------------------------
-    def _read(self, sizehint=-1):
-        if sizehint > self.max_part_size:
-            raise NotImplementedError("Cannot read chunks bigger than a single part yet")
-        if sizehint > 0:
-            data = self._readbuffered(sizehint)
-        else:
-            data = self._readall()
-
-        if len(data) > 0:
-            return data
-        else:
-            return None
-
-    def _write(self, data, flushing=False):
-        def optional_flush(flushable):
-            """Only flush if flushing has been set"""
-            if flushing:
-                flushable.flush()
-
-        if not self.current_part:
-            self._expand_part()
-
-        if self._data_too_big(data):
-            part = self.current_part
-            while self._data_too_big(data):
-                data = part.fill(data)
-                optional_flush(part)
-                if len(data) > 0:
-                    part = self._expand_part()
-            part.fill(data)
-            optional_flush(part)
-        else:
-            self.current_part.write(data)
-            self._fpointer += len(data)
-            optional_flush(self.current_part)
-
-    def _seek(self, offset, whence):
-        if whence > 1:
-            raise NotImplementedError("Only seeking to start is implemented yet.")
-
-        for part in self._parts:
-            part.seek(offset)
-
-        self._fpointer = offset
-
-    def _tell(self):
-        return self._fpointer
-
-    # --------------------------------------------------------------------
-    # Altered behaviour of the FileLikeBase
-    # --------------------------------------------------------------------
-    def close(self):
-        super(PartedFile, self).close()
-        for part in self._parts:
-            part.close()
--- tests/test_partedfs.py
+++ tests/test_partedfs.py
@@ -1,18 +1,45 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import, unicode_literals
+from os import urandom
+
 from mock import Mock, call
 
 from pytest import fixture, raises, mark
+
 from fs.memoryfs import MemoryFS
-from os import urandom
-from drive.partedfs import FilePart, PartedFile, InvalidFilePointerLocation, PartedFS
+
+from drive.partedfs import PartedFS
 from drive.utils import kb
 
 
-class TestPartedFS(FSTestBase):
+class TestPartedFS(object):
     @fixture
-    def fs(self, **kwargs):
+    def fs(self):
         return PartedFS(MemoryFS(), kb(4))
+
+    @fixture
+    def fs_with_folder_structure(self, fs):
+        fs.wrapped_fs.setcontents("backup.tar.part0", data=urandom(kb(4)))
+        fs.wrapped_fs.setcontents("backup.tar.part1", data=urandom(kb(4)))
+        fs.wrapped_fs.setcontents("README.txt.part0", data=urandom(kb(1)))
+        fs.wrapped_fs.makedir("older_backups")
+        return fs
+
+    def test_encode_should_return_file_with_part0_extension(self, fs):
+        #Arrange
+        path = "backup.tar"
+        #Act
+        encoded_path = fs._encode(path)
+        #Assert
+        assert encoded_path == "backup.tar.part0"
+
+    def test_decode_should_return_part_without_part_extension(self, fs):
+        #Arrange
+        encoded_path = "backup.tar.part1"
+        #Act
+        decoded_path = fs._decode(encoded_path)
+        #Assert
+        assert decoded_path == "backup.tar"
 
     def test_listparts_returns_all_parts_for_a_path(self, fs):
         #Arrange
@@ -42,16 +69,6 @@ class TestPartedFS(FSTestBase):
         #Act & Assert
         assert not fs.exists("backup.tar")
 
-    def test_open_for_existing_file_returns_all_parts(self, fs):
-        #Arrange
-        fs.wrapped_fs.setcontents("backup.tar.part0", data=urandom(kb(4)))
-        fs.wrapped_fs.setcontents("backup.tar.part1", data=urandom(kb(4)))
-        fs.wrapped_fs.setcontents("backup.tar.part2", data=urandom(kb(2)))
-        #Act
-        f = fs.open("backup.tar", mode="r")
-        #Assert
-        assert len(f._parts) == 3
-
     def test_remove_deletes_all_parts(self, fs):
         #Arrange
         fs.wrapped_fs.setcontents("backup.tar.part0", data=urandom(kb(4)))
@@ -62,187 +79,24 @@ class TestPartedFS(FSTestBase):
         fs.remove("backup.tar")
         #Assert
         fs.wrapped_fs.remove.assert_has_calls([
-            call("backup.tar.part0"),
-            call("backup.tar.part1"),
-            call("backup.tar.part2")], any_order=True)
+                                                  call("backup.tar.part0"),
+                                                  call("backup.tar.part1"),
+                                                  call("backup.tar.part2")], any_order=True)
 
-
-class TestFilePart:
-    @fixture
-    def file_part(self):
-        fs = MemoryFS()
-        mode = "wb+"
-        return FilePart(wrapped_file=fs.open("backup.tar", mode), mode=mode,
-                        max_size=kb(4))
-
-    def test_fill_with_less_data_returns_none(self, file_part):
+    def test_listdir_returns_only_directories(self, fs_with_folder_structure):
         #Act
-        data = file_part.fill(data=urandom(kb(3)))
+        listing = fs_with_folder_structure.listdir(dirs_only=True)
         #Assert
-        assert data is None
+        assert listing == ["older_backups"]
 
-    def test_fill_with_too_much_data_returns_unwritten_data(self, file_part):
+    def test_listdir_returns_only_files(self, fs_with_folder_structure):
         #Act
-        data = file_part.fill(data=urandom(kb(5)))
+        listing = fs_with_folder_structure.listdir(files_only=True)
         #Assert
-        assert len(data) == kb(1)
+        assert listing == ["README.txt", "backup.tar"]
 
-
-class TestPartedFile:
-    @fixture
-    def parted_file(self):
-        return PartedFile(path="cuckoo.tar", mode="wb+", fs=MemoryFS(), max_part_size=kb(4))
-
-    @fixture
-    def file_parts(self, parted_file):
-        parts = []
-        fs = MemoryFS()
-        for i in range(3):
-            path = "cuckoo.tar.part" + str(i)
-            mode = "wb+"
-            f = fs.open(path, mode)
-            cf = FilePart(f, mode, parted_file.max_part_size)
-            if i == 3:
-                cf.write(urandom(kb(2)))
-            else:
-                cf.write(urandom(parted_file.max_part_size))
-            parts.append(cf)
-
-        return parts
-
-    def test_current_part_returns_none_if_no_parts_exist(self, parted_file):
-        #Act & Assert
-        assert parted_file.current_part is None
-
-    def test_current_part_returns_last_part_when_file_pointer_is_on_last_byte(self, parted_file, file_parts):
-        #Arrange
-        parted_file._parts = file_parts
-        parted_file._fpointer = 2 * kb(4) + kb(2)
-
-        #Act & Assert
-        assert parted_file.current_part == file_parts[2]
-
-    def test_current_part_returns_first_part_when_file_pointer_is_there(self, parted_file, file_parts):
-        #Arrange
-        parted_file._parts = file_parts
-        parted_file._fpointer = 1 * kb(3)
-        #Act & Assert
-        assert parted_file.current_part == file_parts[0]
-
-    def test_current_part_returns_first_part_when_file_pointer_is_max_part_size(self, parted_file, file_parts):
-        #Arrange
-        parted_file._parts = file_parts
-        parted_file._fpointer = 1 * kb(4)
-        #Act & Assert
-        assert parted_file.current_part == file_parts[0]
-
-    def test_current_part_raises_error_when_file_pointer_is_bigger_than_parts(self, parted_file, file_parts):
-        #Arrange
-        parted_file._parts = file_parts
-        parted_file._fpointer = 4 * kb(4)
-        #Act & Assert
-        with raises(InvalidFilePointerLocation):
-            _ = parted_file.current_part
-
-    def test_write_set_file_pointer_to_last_position(self, parted_file):
+    def test_listdir_returns_files_and_directories(self, fs_with_folder_structure):
         #Act
-        parted_file._write(urandom(kb(1)))
+        listing = fs_with_folder_structure.listdir()
         #Assert
-        assert parted_file._fpointer == kb(1)
-
-    def test_write_small_amount_is_written_to_first_part(self, parted_file):
-        #Act
-        parted_file._write(urandom(kb(1)), flushing=True)
-        parted_file._seek(offset=0, whence=1)
-        #Assert
-        assert kb(1) == len(parted_file._parts[0].read())
-
-    def test_write_big_amount_expands_to_parts(self, parted_file):
-        #Act
-        parted_file._write(urandom(kb(12)), flushing=True)
-        parted_file._seek(offset=0, whence=1)
-        #Assert
-        assert len(parted_file._parts) == 3
-
-    def test_read_returns_none_after_all_has_been_read(self, parted_file):
-        #Arrange
-        parted_file._write(urandom(kb(3)), flushing=True)
-        parted_file._seek(offset=0, whence=0)
-        #Act
-        parted_file._read()
-        eof = parted_file._read()
-        #Assert
-        assert eof is None
-
-    def test_read_returns_what_was_written(self, parted_file):
-        #Arrange
-        data = urandom(kb(5))
-        parted_file._write(data, flushing=True)
-        parted_file._seek(offset=0, whence=1)
-        #Act
-        read_data = parted_file._read()
-        #Act & Assert
-        assert len(data) == len(read_data)
-
-    def test_read_raises_error_when_sizehint_is_bigger_than_max_part_size(self, parted_file):
-        #Act & Assert
-        with raises(NotImplementedError):
-            parted_file._read(sizehint=kb(5))
-
-    def test_read_returns_data_with_given_sizehint(self, parted_file):
-        #Arrange
-        parted_file._write(urandom(kb(5)), flushing=True)
-        parted_file._seek(offset=0, whence=0)
-        #Act
-        data = parted_file._read(sizehint=kb(1))
-        #Assert
-        assert len(data) == kb(1)
-
-    def test_read_in_chunks_returns_all_written_data(self, parted_file):
-        #Arrange
-        parted_file._write(urandom(kb(8)), flushing=True)
-        parted_file._seek(offset=0, whence=0)
-        #Act
-        chunk1 = parted_file._read(sizehint=kb(3))
-        chunk2 = parted_file._read(sizehint=kb(3))
-        chunk3 = parted_file._read(sizehint=kb(3))
-        #Assert
-        assert len(chunk1) == kb(3)
-        assert len(chunk2) == kb(3)
-        assert len(chunk3) == kb(2)
-
-    def test_read_returns_none_when_eof_reached(self, parted_file):
-        #Arrange
-        parted_file._write(urandom(kb(4)), flushing=True)
-        parted_file._seek(offset=0, whence=0)
-        #Act
-        parted_file._read(sizehint=kb(2))
-        parted_file._read(sizehint=kb(2))
-        eof = parted_file._read(sizehint=kb(2))
-        #Assert
-        assert eof is None
-
-    def test_seek_can_go_to_start_of_file(self, parted_file):
-        #Arrange
-        parted_file._write(urandom(kb(1)))
-        #Act
-        parted_file._seek(offset=0, whence=0)
-        #Assert
-        assert parted_file._fpointer == 0 and parted_file.current_part == parted_file._parts[0]
-
-    def test_seek_can_go_to_correct_part(self, parted_file):
-        #Arrange
-        parted_file._write(urandom(kb(6)))
-        #Act
-        parted_file._seek(offset=kb(5), whence=0)
-        #Assert
-        assert parted_file._fpointer == kb(5) and parted_file.current_part == parted_file._parts[1]
-
-    def test_tell_returns_file_pointer(self, parted_file):
-        #Arrange
-        parted_file._write(urandom(kb(5)))
-        #Act
-        parted_file._seek(offset=kb(2), whence=0)
-        pos = parted_file._tell()
-        #Assert
-        assert pos == kb(2)
+        assert listing == ["older_backups", "README.txt", "backup.tar"]
