59a380dec7779d8876f30f5b6cecfc39fbc10be1
Lukas Martinelli <me@lukasmartinelli.ch>
2014-04-28 21:24:54 +0200

    Reformated setup.py and tox.ini

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open('README.rst') as fh:
     long_description = fh.read()
 
 setup(
-    name = "cuckoodrive",
-    version = "0.0.1",
-    author = "Lukas Martinelli",
-    author_email = "me@lukasmartinelli.ch",
-    description = ("Aggregates all the free space provided on various "
-                   "cloud storage providers into one big drive."),
+    name="cuckoodrive",
+    version="0.0.1",
+    author="Lukas Martinelli",
+    author_email="me@lukasmartinelli.ch",
+    description=("Aggregates all the free space provided on various \
+                 cloud storage providers into one big drive."),
     license='GPLv2',
     keywords = "fs dropbox",
     url = "https://github.com/lukasmartinelli/cuckoodrive",
